9b536005eca192e0af15cfc1a2d5abe41c7d8cd4
Bogdan Bogdanov <greenwookez@gmail.com>
2023-08-18 01:13:18 +0300

    feat: new page bg

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,25 @@
 import Header from 'pages/components/Header';
 import Footer from 'pages/components/Footer';
-
 import Heading from './components/Heading';
 import Search from './components/Search';
 import DomainList from './components/DomainList';
 import Separator from 'components/Separator/Separator';
 import useSearch from './components/useSearch';
 import Loader from 'components/Loader';
+import { ReactComponent as BgLines } from 'assets/images/bg-lines.svg';
+import { useViewportDimensions } from 'hooks/useViewportDimensions';
 
 const HomePage: React.FC = () => {
   const { isLoading, domains, query } = useSearch();
-
+  const [width] = useViewportDimensions();
   return (
     <div>
+      <BgLines
+        {...{
+          className: 'pointer-events-none block w-full absolute top-0 left-0 right-0',
+          viewBox: width < 1200 ? `0 0 ${width} 549` : `0 0 1600 549`,
+        }}
+      />
       <Header active="" />
       <div className="min-h-[calc(100vh-162px)] max-w-[1000px] mt-[20px] mb-0 mx-auto flex flex-col gap-[50px] max-lg:px-[40px] max-sm:px-[20px] max-sm:gap-[30px]">
         <Heading />
@@ -21,9 +28,9 @@ const HomePage: React.FC = () => {
           isLoading={isLoading}
         />
         <div className="flex flex-col gap-[20px]">
-          {((domains && domains.length > 0) || isLoading) && <Separator />}
+          {domains && domains.length > 0 && <Separator />}
           {isLoading ? (
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center mt-[50px]">
               <Loader />
             </div>
           ) : domains && domains.length > 0 ? (
