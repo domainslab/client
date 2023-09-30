b7d9a605949eea951ab231727229fb430ad79d48
Bogdan Bogdanov <40858992+greenwookez@users.noreply.github.com>
2023-09-30 17:21:11 +0300

    fix: linter error

--- src/pages/components/Footer/Footer.tsx
+++ src/pages/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import Separator from 'components/Separator/Separator';
 import { ReactComponent as PointIcon } from 'assets/icons/point.svg';
 import NavItem from 'components/NavItem';
 // import { ReactComponent as GithubIcon } from 'assets/icons/github.svg';
-import Colors from 'assets/colors/colors';
+// import Colors from 'assets/colors/colors';
 
 const Footer: React.FC = () => (
   <footer className="mt-[60px] px-[20px] sm:px-[40px] md:px-[50px] max-sm:mt-[30px]">
