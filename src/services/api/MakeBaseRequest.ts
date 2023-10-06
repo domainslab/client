1b45acde9c065c8caaa5ed66351ae88cd6aa3990
artemkonovalov <Tema2712>
2023-10-06 15:25:44 +0300

    #1 - fix url

--- src/services/api/MakeBaseRequest.ts
+++ src/services/api/MakeBaseRequest.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
-const BASE_URL = 'http://localhost:3000/v1/';
+const BASE_URL = 'https://api.domainslab.ai/v1/';
 
 export const makeGetBaseRequest = (featureURL, options: {params, headers}) : Promise<AxiosResponse> => {
   return axios
