fa036d9174cf88b1b95431731bef3e42eacbdf27
Bogdan Bogdanov <40858992+greenwookez@users.noreply.github.com>
2023-10-23 13:17:26 +0300

    fix: base url

--- src/services/api/base.ts
+++ src/services/api/base.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { getRequestSignature } from 'utils/getRequestSignature';
 import { RequestOptions } from './types.ts';
 
-const BASE_URL = 'http://localhost:3000/v1/';
+const BASE_URL = 'https://api.domainslab.ai/v1/';
 
 export const get = <T>(featureURL: string, options: RequestOptions): Promise<AxiosResponse<T>> => {
   return axios.get<T>(BASE_URL + featureURL, {
