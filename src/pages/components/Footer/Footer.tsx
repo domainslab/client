1d86525458d1d8742099af7da6d3bcbfd7eabfa9
Dankib16 <110023997+Dankib16@users.noreply.github.com>
2023-11-07 23:15:56 +0200

    Add Contribute button
    Add Contribute button

--- src/pages/components/Footer/Footer.tsx
+++ src/pages/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 import Separator from 'components/Separator/Separator';
 import { ReactComponent as PointIcon } from 'assets/icons/point.svg';
 import NavItem from 'components/NavItem';
-// import { ReactComponent as GithubIcon } from 'assets/icons/github.svg';
-// import Colors from 'assets/colors/colors';
+import { ReactComponent as GithubIcon } from 'assets/icons/github.svg';
+import Colors from 'assets/colors/colors';
 
 const Footer: React.FC = () => (
   <footer className="mt-[60px] px-[20px] sm:px-[40px] md:px-[50px] max-sm:mt-[30px]">
@@ -16,13 +16,15 @@ const Footer: React.FC = () => (
         <li>
           <NavItem to="/policy">Policy</NavItem>
         </li>
-{/*         <PointIcon className="block w-[12px] h-[12px]" />
+
+
+        <PointIcon className="block w-[12px] h-[12px]" />
         <li>
           <NavItem to="https://github.com/domainslab/client" className="flex flex-row items-center gap-2">
             Contribute
             <GithubIcon fill={Colors.TransparentText} />
           </NavItem>
-        </li> */}
+        </li>
       </ul>
       <div className="text-Secondary font-bold tracking-[0.03rem] select-none">domainslab.ai</div>
     </div>
